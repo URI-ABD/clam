e4b488b68521f78b1fe5ab48a5e11c13f3b4c679
Tom <thoward12@outlook.com>
2020-03-01 14:25:05 -0500

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -10,25 +10,35 @@ pub use crate::manifold::Manifold;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use types::*;
-    use ndarray::{ArrayBase, Array2, array};
+    use ndarray::*;
+    use types::{Data};
     use crate::dataset::Dataset;
+
+    type Arr = Array2<f32>;
+
+    fn line(n: usize, m: f32, b: f32) -> Arr {
+        let mut arr = Arr::zeros((n, 2));
+        // Build Xs
+        let mut pointer = arr.column_mut(0);
+        let mut xs = Array::range(0., n as f32, 1.);
+        pointer = xs.view_mut(); 
+        // Build Yx 
+        let mut pointer = arr.column_mut(1);
+        let mut ys = m * xs + b;
+        pointer = ys.view_mut();
+
+        arr
+    }
 
     #[test]
     fn test_line() {
-        // let xs = Array1::<f32>::zeros(8);
-        // let ys = array![[0f32..8f32]];
-        // let line: Array2<f32> = xs.iter().zip(ys.iter()).map(|(x, y)| (x, y)).collect();
-
-        let mut line = Array2::<f32>::zeros((8, 2));
-        let ys = ArrayBase::<f32>::range(0, 8, 1);
-        line.column_mut(1) = ys;
+        let line = line(8, 0., 0.);
         let data = Data::from(line);
         let dataset = Dataset::new(data, "euclidean");
         let criteria = vec![criteria::MinPoints::new(1)];
         let manifold = Manifold::new(dataset, criteria);
 
-        assert_eq!(manifold.leaves(Some(3)), 8);
+        assert_eq!(manifold.leaves(Some(3)).len(), 8);
     }
 
     #[test]
