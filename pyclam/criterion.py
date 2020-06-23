ecaed00897f6a27dd785415bf62fe1a5366f8b21
Najib Ishaq <najib_ishaq@my.uri.edu>
2020-06-22 21:14:04 -0400

    feat: SelectionCriterion and Graph Criterion
    feat: added LFDRange as SelectionCriterion

--- pyclam/criterion.py
+++ pyclam/criterion.py
@@ -1,27 +1,38 @@
 import logging
 from abc import ABC, abstractmethod
+from typing import Set, Tuple, List
 
 import numpy as np
 from scipy.spatial.distance import cdist
 
 from pyclam.manifold import Cluster, Graph
 
+# TODO: class ChildTooSmall which checks % of parent owned by child, relative populations of child parent and root.
+# TODO: RE above, Sparseness which looks at % owned / radius or similar
+
 
 class Criterion(ABC):
     pass
 
 
-class GraphCriterion(Criterion):
-
-    @abstractmethod
-    def __call__(self, graph: Graph):
-        pass
-
-
 class ClusterCriterion(Criterion):
 
     @abstractmethod
-    def __call__(self, cluster: Cluster):
+    def __call__(self, cluster: Cluster) -> bool:
+        pass
+
+
+class SelectionCriterion(Criterion):
+
+    @abstractmethod
+    def __call__(self, root: Cluster) -> Set[Cluster]:
+        pass
+
+
+class GraphCriterion(Criterion):
+
+    @abstractmethod
+    def __call__(self, graph: Graph) -> Graph:
         pass
 
 
@@ -32,7 +43,7 @@ class MaxDepth(ClusterCriterion):
     def __init__(self, depth):
         self.depth = depth
 
-    def __call__(self, cluster: Cluster):
+    def __call__(self, cluster: Cluster) -> bool:
         return cluster.depth < self.depth
 
 
@@ -44,7 +55,7 @@ class AddLevels(ClusterCriterion):
         self.depth = depth
         self.start = None
 
-    def __call__(self, cluster: Cluster):
+    def __call__(self, cluster: Cluster) -> bool:
         if self.start is None:
             self.start = cluster.depth
         return cluster.depth < (self.start + self.depth)
@@ -57,75 +68,19 @@ class MinPoints(ClusterCriterion):
     def __init__(self, points):
         self.min_points = points
 
-    def __call__(self, cluster: Cluster):
+    def __call__(self, cluster: Cluster) -> bool:
         return cluster.cardinality > self.min_points
 
 
-# class MinRadius(ClusterCriterion):
-#     """ Allows clustering until cluster.radius is less than radius.
-#     """
-#
-#     def __init__(self, radius):
-#         self.radius = radius
-#         MIN_RADIUS = radius
-#
-#     def __call__(self, cluster: Cluster):
-#         if cluster.radius <= self.radius:
-#             return False
-#         return True
+class MinRadius(ClusterCriterion):
+    """ Allows clustering until cluster.radius is less than radius.
+    """
 
+    def __init__(self, radius):
+        self.radius = radius
 
-# class LeavesSubgraph(ClusterCriterion):
-#     """ Allows clustering until the cluster has left the subgraph of the parent.
-#     """
-#
-#     def __init__(self, manifold: _Manifold):
-#         self.manifold = manifold
-#         return
-#
-#     def __call__(self, cluster: _Cluster):
-#         parent_subgraph = self.manifold.layers[cluster.depth - 1].subgraph(self.manifold.select(cluster.name[:-1]))
-#         return any((c.overlaps(cluster.medoid, cluster.radius) for c in parent_subgraph))
-
-
-# class MinCardinality(ClusterCriterion):
-#     """ Allows clustering until cardinality of cluster's subgraph is less than given.
-#     """
-#
-#     def __init__(self, cardinality):
-#         self.cardinality = cardinality
-#
-#     def __call__(self, cluster: _Cluster):
-#         return any((
-#             # If there are fewer points than needed, we don't check cardinality.
-#             cluster.manifold.layers[cluster.depth].cardinality <= self.cardinality,
-#             cluster.manifold.layers[cluster.depth].subgraph(cluster).cardinality >= self.cardinality
-#         ))
-
-
-# class MinNeighborhood(ClusterCriterion):
-#     """ Allows clustering until the size of the neighborhood drops below threshold.
-#     """
-#
-#     def __init__(self, starting_depth: int, threshold: int):
-#         self.starting_depth = starting_depth
-#         self.threshold = threshold
-#         return
-#
-#     def __call__(self, cluster: _Cluster) -> bool:
-#         return cluster.depth < self.starting_depth or len(cluster.neighbors) >= self.threshold
-
-
-# class NewSubgraph(ClusterCriterion):
-#     """ Cluster until a new subgraph is created. """
-#
-#     def __init__(self, manifold: _Manifold):
-#         self.manifold = manifold
-#         self.starting = len(manifold.layers[-1].subgraphs)
-#         return
-#
-#     def __call__(self, _):
-#         return len(self.manifold.layers[-1].subgraphs) == self.starting
+    def __call__(self, cluster: Cluster) -> bool:
+        return cluster.radius > self.radius
 
 
 class MedoidNearCentroid(ClusterCriterion):
@@ -154,5 +109,79 @@ class UniformDistribution(ClusterCriterion):
         distance = wasserstein_distance(freq, ideal)
         return distance > 0.25
 
-# TODO: class ChildTooSmall which checks % of parent owned by child, relative populations of child parent and root.
-# TODO: RE above, Sparseness which looks at % owned / radius or similar
+
+class LFDRange(SelectionCriterion):
+
+    def __init__(self, percentiles: Tuple[float, float]):
+        if len(percentiles) != 2:
+            raise ValueError(f'LFDRange criterion expected two percentile values. '
+                             f'Got: {len(percentiles)}')
+        elif percentiles[0] < 0. or percentiles[0] > 100.:
+            raise ValueError(f'LFDRange criterion expected upper percentile to be between 0 and 100. '
+                             f'Got: {percentiles[0]}')
+        elif percentiles[1] < 0. or percentiles[1] > 100.:
+            raise ValueError(f'LFDRange criterion expected lower percentile to be between 0 and 100. '
+                             f'Got: {percentiles[1]}')
+
+        self.upper: float = float(percentiles[0])
+        self.lower: float = float(percentiles[1])
+
+    def __call__(self, root: Cluster) -> Set[Cluster]:
+        upper, lower, grace_depth = self._range(root)
+        return self._select(root, upper, lower, grace_depth)
+
+    def _range(self, root: Cluster) -> Tuple[float, float, int]:
+        manifold = root.manifold
+        lfd_range = [], []
+        for depth in range(1, manifold.depth):
+            if manifold.layers[depth + 1].cardinality < 2 ** (depth + 1):
+                clusters: List[Cluster] = [cluster for cluster in manifold.layers[depth] if cluster.cardinality > 2]
+                if len(clusters) > 0:
+                    lfds = np.percentile(
+                        a=[c.local_fractal_dimension for c in clusters],
+                        q=[self.upper, self.lower],
+                    )
+                    lfd_range[0].append(lfds[0]), lfd_range[1].append(lfds[1])
+        if len(lfd_range[0]) > 0:
+            upper: float = float(np.median(lfd_range[0]))
+            lower: float = float(np.median(lfd_range[1]))
+            depth: int = manifold.depth - len(lfd_range[0])
+            return upper, lower, depth
+        else:
+            lfds = np.percentile(
+                a=[cluster.local_fractal_dimension for cluster in manifold.layers[-1]],
+                q=[self.upper, self.lower],
+            )
+            return float(lfds[0]), float(lfds[1]), manifold.depth
+        pass
+
+    @staticmethod
+    def _select(root: Cluster, upper: float, lower: float, grace_depth: int) -> Set[Cluster]:
+        selected: Set[Cluster] = set()
+        active: Set[Cluster] = set()
+        inactive: Set[Cluster] = {cluster for cluster in root.manifold.layers[grace_depth].edges}
+
+        while active or inactive:
+            # Select childless clusters
+            # TODO: This should only be needed for clusters in inactive set. Check to make sure.
+            childless: Set[Cluster] = {cluster for cluster in inactive if len(cluster.children) == 0}
+            childless.update({cluster for cluster in active if len(cluster.children) == 0})
+            inactive -= childless
+            active -= childless
+            selected += childless
+
+            # Select clusters that fall below the lower threshold
+            selections: Set[Cluster] = {cluster for cluster in active if cluster.local_fractal_dimension <= lower}
+            active -= selections
+            selected += selections
+
+            # activate branches that rise above the upper threshold
+            activations: Set[Cluster] = {cluster for cluster in inactive if cluster.local_fractal_dimension >= upper}
+            inactive -= activations
+            active += activations
+
+            # replace active and inactive sets with child clusters
+            active = {child for cluster in active for child in cluster.children}
+            inactive = {child for cluster in inactive for child in cluster.children}
+
+        return selected
