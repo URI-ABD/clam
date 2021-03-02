72970bd623f5e966b502ae2368b6d1334ea1272e
Najib Ishaq <najib_ishaq@uri.edu>
2021-03-02 03:01:01 -0500

    feat: dataset as a trait

--- src/dataset.rs
+++ src/dataset.rs
@@ -2,38 +2,121 @@
 ///
 /// This module contains the declaration and definition of the Dataset struct.
 use std::{fmt, result};
+use std::fmt::Debug;
+use std::sync::Arc;
 
 use dashmap::DashMap;
-use ndarray::{Array2, ArrayView1};
+use ndarray::{Array2, ArrayView, Axis, IxDyn};
 use rand::seq::IteratorRandom;
-use rand::seq::SliceRandom;
 use rayon::prelude::*;
 
-use crate::metric::{metric_new, Metric, Number};
+use crate::metric::{Metric, metric_new, Number};
 use crate::types::{Index, Indices};
 
-/// Dataset.
+/// Dataset Trait.
+///
+/// All datasets supplied to CLAM must implement this trait.
+///
+pub trait Dataset<T, U>: Debug + Send + Sync {
+    /// Returns the name of the metric used to compute the distance between instances.
+    ///
+    /// :warning: This name must be available in the distances crate.
+    fn metric(&self) -> &'static str; // should this return the function directly?
+
+    /// Returns the number of instances in the dataset.
+    fn ninstances(&self) -> usize;
+
+    /// Returns the shape of the dataset.
+    fn shape(&self) -> &[usize];
+
+    /// Returns the Indices for the dataset.
+    fn indices(&self) -> Indices;
+
+    /// Returns the instance at the given index.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - The index of the instance to return in the dataset.
+    ///
+    fn instance(&self, index: Index) -> Arc<ArrayView<T, IxDyn>>;
+
+    /// Selects `n` unique instances from the given indices and returns their indices.
+    ///
+    /// # Arguments
+    ///
+    /// * `indices` - The indices to select instances from
+    /// * `n` - The number of indices to return
+    ///
+    fn choose_unique(&self, indices: Indices, n: usize) -> Indices;
+
+    /// Computes the distance between the two instances at the indices provided.
+    ///
+    /// # Arguments
+    ///
+    /// * `left` - Index of an instance to compute distance from
+    /// * `right`- Index of an instance to compute distance from
+    ///
+    fn distance(&self, left: Index, right: Index) -> U;
+
+    /// Computes the distances from the instances at left to right.
+    ///
+    /// # Arguments
+    ///
+    /// * `left` - Index of the instance to compute distances from
+    /// * `right` - Indices of the instances to compute distances to
+    #[allow(clippy::ptr_arg)]
+    fn distances_from(&self, left: Index, right: &Indices) -> Vec<U>;
+
+    /// Computes and returns distances amongst the instances at left and right.
+    ///
+    /// # Arguments
+    ///
+    /// * `left` - Reference to the indices of instances to compute distances among
+    /// * `right` - Reference to the indices of instances to compute distances among
+    #[allow(clippy::ptr_arg)]
+    fn distances_among(&self, left: &Indices, right: &Indices) -> Vec<Vec<U>>;
+
+    /// Computes and returns the pairwise distances between the instances at the given indices.
+    ///
+    /// # Arguments
+    ///
+    /// * `indices` - Reference to indices of instances to compute pairwise distances on.
+    #[allow(clippy::ptr_arg)]
+    fn pairwise_distances(&self, indices: &Indices) -> Vec<Vec<U>>;
+
+    /// Clears the dataset cache.
+    fn clear_cache(&self) {}
+
+    /// Returns the size of the cache used for the dataset.
+    fn cache_size(&self) -> Option<usize>;
+}
+
+/// RowMajor dataset represented as a 2-dimensional array where rows are instances and columns are attributes.
 ///  
-/// Datasets wrap an `Array2` of data, along with a `metric`, to provide interfaces
+/// A wrapper around an `Array2` of data, along with a provided `metric`, to provide an interface
 /// for computing distances between points contained within the dataset.
 ///
 /// The resulting structure can make use of caching techniques to prevent repeated (potentially expensive)
 /// calls to its internal distance function.
-pub struct Dataset<T: Number, U: Number> {
+pub struct RowMajor<T: Number, U: Number> {
     /// 2D array of data
     pub data: Array2<T>,
+
+    // TODO: Remove the string name and rely only on a closure (Metric<T, U>).
     /// Metric to use to compute distances (ex "euclidean")
     pub metric: &'static str,
+
     /// Whether this dataset should use an internal cache (recommended)
     pub use_cache: bool,
 
     // The stored function, used to compute distances.
     function: Metric<T, U>,
+
     // The internal cache.
     cache: DashMap<(Index, Index), U>,
 }
 
-impl<T: Number, U: Number> fmt::Debug for Dataset<T, U> {
+impl<T: Number, U: Number> fmt::Debug for RowMajor<T, U> {
     fn fmt(&self, f: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
         f.debug_struct("Dataset")
             .field("data-shape", &self.data.shape())
@@ -43,14 +126,14 @@ impl<T: Number, U: Number> fmt::Debug for Dataset<T, U> {
     }
 }
 
-impl<T: Number, U: Number> Dataset<T, U> {
+impl<T: Number, U: Number> RowMajor<T, U> {
     /// Create a new Dataset, using the provided data and metric, optionally use a cache.
     pub fn new(
         data: Array2<T>,
         metric: &'static str,
         use_cache: bool,
-    ) -> Result<Dataset<T, U>, String> {
-        Ok(Dataset {
+    ) -> Result<RowMajor<T, U>, String> {
+        Ok(RowMajor {
             data,
             metric,
             use_cache,
@@ -58,49 +141,48 @@ impl<T: Number, U: Number> Dataset<T, U> {
             cache: DashMap::new(),
         })
     }
+}
 
-    /// Return all of the indices in the dataset.
-    pub fn indices(&self) -> Indices {
-        (0..self.data.shape()[0]).collect()
+impl<T: Number, U: Number> Dataset<T, U> for RowMajor<T, U> {
+    /// Return the metric name for the dataset.
+    fn metric(&self) -> &'static str {
+        self.metric
     }
 
     /// Returns the number of rows in the dataset.
-    pub fn nrows(&self) -> usize {
+    fn ninstances(&self) -> usize {
         self.data.nrows()
     }
 
     /// Returns the shape of the dataset.
-    pub fn shape(&self) -> &[usize] {
+    fn shape(&self) -> &[usize] {
         self.data.shape()
     }
 
-    /// Clears the internal cache.
-    pub fn clear_cache(&self) {
-        self.cache.clear()
-    }
-
-    /// Returns the size of the internal cache.
-    pub fn cache_size(&self) -> usize {
-        self.cache.len()
-    }
-
-    /// Return a random selection of unique indices.
-    /// 
-    /// Returns `n` unique random indices from the provided vector.
-    /// If `n` is greater than the number of indices provided, the full list is returned in shuffled order.
-    #[allow(clippy::ptr_arg)]
-    pub fn choose_unique(&self, indices: Indices, n: usize) -> Indices {
-        // TODO: actually check for uniqueness among choices
-        indices.into_iter().choose_multiple(&mut rand::thread_rng(), n)
+    /// Return all of the indices in the dataset.
+    fn indices(&self) -> Indices {
+        (0..self.data.shape()[0]).collect()
     }
 
     /// Return the row at the provided index.
-    pub fn row(&self, i: Index) -> ArrayView1<T> {
-        self.data.row(i)
+    fn instance(&self, i: Index) -> Arc<ArrayView<T, IxDyn>> {
+        Arc::new(self.data.index_axis(Axis(0), i).into_dyn())
+    }
+
+    /// Return a random selection of unique indices.
+    ///
+    /// Returns `n` unique random indices from the provided vector.
+    /// If `n` is greater than the number of indices provided, the full list is returned in shuffled order.
+    #[allow(clippy::ptr_arg)]
+    fn choose_unique(&self, indices: Indices, n: usize) -> Indices {
+        // TODO: actually check for uniqueness among choices
+        indices
+            .into_iter()
+            .choose_multiple(&mut rand::thread_rng(), n)
     }
 
     /// Compute the distance between `left` and `right`.
-    pub fn distance(&self, left: Index, right: Index) -> U {
+    fn distance(&self, left: Index, right: Index) -> U {
         if left == right {
             U::zero()
         } else {
@@ -112,7 +194,10 @@ impl<T: Number, U: Number> Dataset<T, U> {
             if !self.cache.contains_key(&key) {
                 self.cache.insert(
                     key,
-                    (self.function)(self.data.row(left), self.data.row(right)),
+                    (self.function)(
+                        Arc::new(self.data.row(left).into_dyn()),
+                        Arc::new(self.data.row(right).into_dyn()),
+                    ),
                 );
             }
             *self.cache.get(&key).unwrap()
@@ -121,7 +206,7 @@ impl<T: Number, U: Number> Dataset<T, U> {
 
     /// Compute the distances from `left` to all points in `right`.
     #[allow(clippy::ptr_arg)]
-    pub fn distances_from(&self, left: Index, right: &Indices) -> Vec<U> {
+    fn distances_from(&self, left: Index, right: &Indices) -> Vec<U> {
         right
             .par_iter()
             .map(|&r| self.distance(left, r))
@@ -130,7 +215,7 @@ impl<T: Number, U: Number> Dataset<T, U> {
 
     /// Compute distances between all points in `left` and `right`.
     #[allow(clippy::ptr_arg)]
-    pub fn distances_among(&self, left: &Indices, right: &Indices) -> Vec<Vec<U>> {
+    fn distances_among(&self, left: &Indices, right: &Indices) -> Vec<Vec<U>> {
         left.par_iter()
             .map(|&l| self.distances_from(l, right))
             .collect::<Vec<Vec<U>>>()
@@ -138,24 +223,38 @@ impl<T: Number, U: Number> Dataset<T, U> {
 
     /// Compute the pairwise distance between all points in `indices`.
     #[allow(clippy::ptr_arg)]
-    pub fn pairwise_distances(&self, indices: &Indices) -> Vec<Vec<U>> {
+    fn pairwise_distances(&self, indices: &Indices) -> Vec<Vec<U>> {
         self.distances_among(indices, indices)
+    }
+
+    /// Clears the internal cache.
+    fn clear_cache(&self) {
+        self.cache.clear()
+    }
+
+    /// Returns the number of elements in the internal cache.
+    fn cache_size(&self) -> Option<usize> {
+        Some(self.cache.len())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use float_cmp::approx_eq;
     use ndarray::prelude::*;
 
     use super::Dataset;
+    use super::RowMajor;
 
     #[test]
     fn test_dataset() {
         let data: Array2<f64> = array![[1., 2., 3.], [3., 3., 1.]];
-        let dataset = Dataset::new(data, "euclidean", false).unwrap();
-        assert_eq!(dataset.nrows(), 2);
-        assert_eq!(dataset.row(0), array![1., 2., 3.]);
+        let row_0 = array![1., 2., 3.].into_dyn();
+        let dataset = RowMajor::new(data, "euclidean", false).unwrap();
+        assert_eq!(dataset.ninstances(), 2);
+        assert_eq!(Arc::try_unwrap(dataset.instance(0)).unwrap(), row_0,);
 
         approx_eq!(f64, dataset.distance(0, 0), 0.);
         approx_eq!(f64, dataset.distance(0, 1), 3.);
@@ -166,7 +265,7 @@ mod tests {
     #[test]
     fn test_choose_unique() {
         let data: Array2<f64> = array![[1., 2., 3.], [3., 2., 1.]];
-        let dataset: Dataset<f64, f64> = Dataset::new(data, "euclidean", false).unwrap();
+        let dataset: RowMajor<f64, f64> = RowMajor::new(data, "euclidean", false).unwrap();
         assert_eq!(dataset.choose_unique(vec![0], 1), [0]);
         assert_eq!(dataset.choose_unique(vec![0], 5), [0]);
         assert_eq!(dataset.choose_unique(vec![0, 1], 1).len(), 1);
--- src/metric.rs
+++ src/metric.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 use std::fmt::{Debug, Display};
-use std::iter::{FromIterator, Sum};
+use std::iter::Sum;
+use std::sync::Arc;
 
-use ndarray::{ArrayView1, Zip};
-use ndarray::parallel::prelude::*;
+use ndarray::{ArrayView, IxDyn};
 use num_traits::{Num, NumCast};
 
 pub trait Number: Num + Clone + Copy + NumCast + PartialOrd + Send + Sync + Sum + Debug + Display {}
@@ -22,17 +22,13 @@ impl Number for i64 {}
 impl Number for i128 {}
 impl Number for isize {}
 
-
-pub type Metric<T, U> = fn(ArrayView1<T>, ArrayView1<T>) -> U;
+pub type Metric<T, U> = fn(Arc<ArrayView<T, IxDyn>>, Arc<ArrayView<T, IxDyn>>) -> U;
 
 pub fn metric_new<T: Number, U: Number>(metric: &'static str) -> Result<Metric<T, U>, String> {
     match metric {
         "euclidean" => Ok(euclidean),
-        "par_euclidean" => Ok(par_euclidean),
         "euclideansq" => Ok(euclideansq),
-        "par_euclideansq" => Ok(par_euclideansq),
         "manhattan" => Ok(manhattan),
-        "par_manhattan" => Ok(par_manhattan),
         "cosine" => Ok(cosine),
         "hamming" => Ok(hamming),
         "jaccard" => Ok(jaccard),
@@ -40,89 +36,85 @@ pub fn metric_new<T: Number, U: Number>(metric: &'static str) -> Result<Metric<T
     }
 }
 
-fn euclidean<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
+fn euclidean<T: Number, U: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> U {
     let d: f64 = NumCast::from(euclideansq::<T, U>(x, y)).unwrap();
     U::from(d.sqrt()).unwrap()
 }
 
-fn par_euclidean<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d: f64 = NumCast::from(par_euclideansq::<T, U>(x, y)).unwrap();
-    U::from(d.sqrt()).unwrap()
-}
-
-fn euclideansq<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d: T = x.iter()
+fn euclideansq<T: Number, U: Number>(
+    x: Arc<ArrayView<T, IxDyn>>,
+    y: Arc<ArrayView<T, IxDyn>>,
+) -> U {
+    let d: T = x
+        .iter()
         .zip(y.iter())
         .map(|(&a, &b)| (a - b) * (a - b))
         .sum();
     U::from(d).unwrap()
 }
 
-fn par_euclideansq<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d: T = Zip::from(x)
-        .and(y)
-        .into_par_iter()
-        .map(|(&a, &b)| (a - b) * (a - b))
-        .sum();
-    U::from(d).unwrap()
-}
-
-fn par_manhattan<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d: T = Zip::from(x)
-        .and(y)
-        .into_par_iter()
-        .map(|(&a, &b)| if a > b { a - b } else { b - a })
-        .sum();
-    U::from(d).unwrap()
-}
-
-fn manhattan<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d: T = x.iter()
+fn manhattan<T: Number, U: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> U {
+    let d: T = x
+        .iter()
         .zip(y.iter())
         .map(|(&a, &b)| if a > b { a - b } else { b - a })
         .sum();
     U::from(d).unwrap()
 }
 
-fn dot<T: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> T {
+fn dot<T: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> T {
     x.iter().zip(y.iter()).map(|(&a, &b)| a * b).sum()
 }
 
-fn cosine<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let xx = dot(x, x);
-    if xx == T::zero() { return U::one(); }
+#[allow(clippy::suspicious_operation_groupings)]
+fn cosine<T: Number, U: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> U {
+    let xx = dot(Arc::clone(&x), Arc::clone(&x));
+    if xx == T::zero() {
+        return U::one();
+    }
 
-    let yy = dot(y, y);
-    if yy == T::zero() { return U::one(); }
+    let yy = dot(Arc::clone(&y), Arc::clone(&y));
+    if yy == T::zero() {
+        return U::one();
+    }
 
-    let xy = dot(x, y);
-    if xy <= T::zero() { return U::one() }
+    let xy = dot(Arc::clone(&x), Arc::clone(&y));
+    if xy <= T::zero() {
+        return U::one();
+    }
 
     let similarity: f64 = NumCast::from(xy * xy / (xx * yy)).unwrap();
     U::one() - U::from(similarity.sqrt()).unwrap()
 }
 
-fn hamming<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    let d = x.iter().zip(y.iter()).filter(|(&a, &b)| a != b ).count();
+fn hamming<T: Number, U: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> U {
+    let d = x.iter().zip(y.iter()).filter(|(&a, &b)| a != b).count();
     U::from(d).unwrap()
 }
 
-fn jaccard<T: Number, U: Number>(x: ArrayView1<T>, y: ArrayView1<T>) -> U {
-    if x.is_empty() || y.is_empty() { return U::one(); }
+fn jaccard<T: Number, U: Number>(x: Arc<ArrayView<T, IxDyn>>, y: Arc<ArrayView<T, IxDyn>>) -> U {
+    if x.is_empty() || y.is_empty() {
+        return U::one();
+    }
 
     let x: HashSet<u64> = x.iter().map(|&a| NumCast::from(a).unwrap()).collect();
-    let intersect = y.iter().filter(|&&b| x.contains(&NumCast::from(b).unwrap())).count();
+    let intersect = y
+        .iter()
+        .filter(|&&b| x.contains(&NumCast::from(b).unwrap()))
+        .count();
 
-    if intersect == x.len() && intersect == y.len() { return U::zero() }
+    if intersect == x.len() && intersect == y.len() {
+        return U::zero();
+    }
 
-    U::one() -  U::from(intersect).unwrap() / U::from(x.len() + y.len() - intersect).unwrap()
+    U::one() - U::from(intersect).unwrap() / U::from(x.len() + y.len() - intersect).unwrap()
 }
-
 
 #[cfg(test)]
 mod tests {
     use float_cmp::approx_eq;
     use ndarray::{arr2, Array2};
+    use std::sync::Arc;
 
     use crate::metric::metric_new;
 
@@ -131,16 +123,58 @@ mod tests {
         let data: Array2<f64> = arr2(&[[1., 2., 3.], [3., 3., 1.]]);
 
         let distance = metric_new("euclideansq").unwrap();
-        approx_eq!(f64, distance(data.row(0), data.row(0)), 0.);
-        approx_eq!(f64, distance(data.row(0), data.row(1)), 9.);
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(0).into_dyn())
+            ),
+            0.
+        );
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(1).into_dyn())
+            ),
+            9.
+        );
 
         let distance = metric_new("euclidean").unwrap();
-        approx_eq!(f64, distance(data.row(0), data.row(0)), 0.);
-        approx_eq!(f64, distance(data.row(0), data.row(1)), 3.);
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(0).into_dyn())
+            ),
+            0.
+        );
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(1).into_dyn())
+            ),
+            3.
+        );
 
         let distance = metric_new("manhattan").unwrap();
-        approx_eq!(f64, distance(data.row(0), data.row(0)), 0.);
-        approx_eq!(f64, distance(data.row(0), data.row(1)), 5.);
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(0).into_dyn())
+            ),
+            0.
+        );
+        approx_eq!(
+            f64,
+            distance(
+                Arc::new(data.row(0).into_dyn()),
+                Arc::new(data.row(1).into_dyn())
+            ),
+            5.
+        );
     }
 
     #[test]
