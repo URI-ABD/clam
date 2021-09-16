af02725dc4ee4bddf7a495e75f548b1273ee70ca
Tom <info@tomhoward.codes>
2021-09-15 21:32:59 -0400

    build: migrate python from pyproject.toml to setup.py
    We made this change so we could dynamically fetch version numbers from the rust Cargo.toml.
    Python is a second-class citizen.
    
    Co-authored-by: Najib <nishaq503@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
+"""CLAM Python Setup.
+
+This file pulls all important information from elsewhere.
+If you are changing something here, you probably went wrong in life earlier.
+"""
 import os
-import toml
 
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+DIR = os.path.dirname(__file__)
+
+# Get description from the README.
+with open(os.path.join(DIR, 'README.md'), 'r') as f:
     long_description = f.read()
 
-cargo = toml.load('Cargo.toml')
+# Pull version number from Rust, because Rust is Truth.
+with open(os.path.join(DIR, 'Cargo.toml'), 'r') as f:
+    contents = f.readlines()
+
+version = list(filter(lambda l: l.startswith('version'), contents))
+assert len(version) == 1, f"Multiple versions found! {version}"
+version = version[0].split('=')[1].strip()[1:-1]
+assert version[0] != '"' and version[-1] != '"', f"Version seems to still have quotes! {version}"
+assert len(version.split('.')) == 3, f"Version contains too many parts! {version}"
+
+# Put it all together!
 setup(
     name='pyclam',
-    version=cargo['package']['version'],
+    version=version,
     packages=['pyclam'],
     url='https://github.com/URI-ABD/clam',
     license='MIT',
-    author='; '.join(cargo['package']['authors']),
     author_email='',
     description='Clustered Learning of Approximate Manifolds',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['numpy', 'scipy', 'toml'],
+    install_requires=['numpy==1.*', 'scipy==1.*'],
     python_requires='>=3.6',
 )
