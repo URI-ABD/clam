2212d4baeaf7b409cba0bc78d422828b177edae4
Najib Ishaq <najib_ishaq@zoho.com>
2023-01-20 19:16:28 -0500

    wip: benching partition with new geometry

--- benches/partition.rs
+++ benches/partition.rs
@@ -19,25 +19,19 @@ fn partition(c: &mut Criterion) {
         let (features, _) = anomaly_readers::read_anomaly_data(data_name, true).unwrap();
 
         let dataset = clam::Tabular::new(&features, data_name.to_string());
-        if dataset.cardinality() > 50_000 {
-            continue;
-        }
+        // if dataset.cardinality() > 50_000 {
+        //     continue;
+        // }
 
         let euclidean = metric_from_name::<f32, f32>("euclidean", false).unwrap();
         // let log_cardinality = (dataset.cardinality() as f64).log2() as usize;
         let partition_criteria = clam::PartitionCriteria::new(true).with_min_cardinality(1);
 
-        for use_cache in [false, true] {
-            if use_cache {
-                continue;
-            }
-
-            let bench_name = format!("{}-distance-cache-{}", data_name, use_cache);
-            group.bench_with_input(&bench_name, &use_cache, |b, &use_cache| {
-                let space = clam::TabularSpace::new(&dataset, euclidean.as_ref(), use_cache);
-                b.iter_with_large_drop(|| Cluster::new_root(&space).build().partition(&partition_criteria, true))
-            });
-        }
+        let bench_name = format!("{}-{}-{}", data_name, dataset.cardinality(), dataset.dimensionality());
+        group.bench_function(&bench_name, |b| {
+            let space = clam::TabularSpace::new(&dataset, euclidean.as_ref(), false);
+            b.iter_with_large_drop(|| Cluster::new_root(&space).build().partition(&partition_criteria, true))
+        });
     }
 
     group.finish();
