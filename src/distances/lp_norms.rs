df8bc89d66f71c0acd6b0711919ac5c621b60dc4
Noah Daniels <ndaniels@mac.com>
2023-07-02 12:06:28 -0400

    l3 norm implemented

--- src/distances/lp_norms.rs
+++ src/distances/lp_norms.rs
@@ -18,6 +18,18 @@ pub fn manhattan<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
     U::from(d).unwrap()
 }
 
+/// Lebesgue L3 norm
+pub fn l3_norm<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
+    let d: T = x
+        .iter()
+        .zip(y.iter())
+        .map(|(&a, &b)| if a > b { a - b } else { b - a })
+        .map(|v| v * v * v)
+        .sum();
+    let d = d.as_f64().cbrt();
+    U::from(d).unwrap()
+}
+
 pub fn l4_norm<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
     let d: T = x
         .iter()
