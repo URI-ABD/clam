eaa732bc992944e791be6de9806a920758d325f0
Najib Ishaq <najib_ishaq@zoho.com>
2023-07-10 14:04:23 -0400

    chore: updated benchmarks for search

--- benches/rnn-search.rs
+++ benches/rnn-search.rs
@@ -10,37 +10,45 @@ use abd_clam::{
 };
 
 fn cakes(c: &mut Criterion) {
-    for &(metric_name, metric) in COMMON_METRICS_F32 {
+    let seed = 42;
+    let (cardinality, dimensionality) = (100_000, 100);
+    let (min_val, max_val) = (-1., 1.);
+
+    let data = random_data::random_f32(cardinality, dimensionality, min_val, max_val, seed);
+    let data = data.iter().map(|v| v.as_slice()).collect::<Vec<_>>();
+
+    let num_queries = 1_000;
+    let queries = random_data::random_f32(num_queries, dimensionality, min_val, max_val, seed + 1);
+    let queries = queries.iter().map(|v| v.as_slice()).collect::<Vec<_>>();
+
+    for &(metric_name, metric) in &COMMON_METRICS_F32[..1] {
         let mut group = c.benchmark_group(format!("rnn-{metric_name}"));
-        group.significance_level(0.025).sample_size(10);
 
         let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
         group.plot_config(plot_config);
 
         group.sampling_mode(SamplingMode::Flat);
 
-        let num_queries = 10_000;
         group.throughput(Throughput::Elements(num_queries as u64));
 
-        let seed = 42;
-        let (dimensionality, min_val, max_val) = (10, 0., 1.);
-        let data = random_data::random_f32(100_000, dimensionality, min_val, max_val, seed);
-        let queries = random_data::random_f32(num_queries, dimensionality, min_val, max_val, seed);
-        let data = data.iter().map(|v| v.as_slice()).collect::<Vec<_>>();
-        let queries = queries.iter().map(|v| v.as_slice()).collect::<Vec<_>>();
-
-        let dataset = VecVec::new(data, metric, "100k-10".to_string(), false);
+        let dataset = VecVec::new(data.clone(), metric, "rnn".to_string(), false);
         let criteria = PartitionCriteria::new(true).with_min_cardinality(1);
         let cakes = CAKES::new(dataset, Some(seed), criteria);
 
-        for n in (0..=100).step_by(10) {
-            let radius = (n as f32) / if metric_name == "cosine" { 10_000. } else { 1_000. };
-
-            let id = BenchmarkId::new("100k-10", radius);
-            group.bench_with_input(id, &n, |b, _| {
+        let mut radius = 0.;
+        for n in (0..=100).step_by(25) {
+            radius = (n as f32) / if metric_name == "cosine" { 10_000. } else { 1_000. };
+            let id = BenchmarkId::new("Clustered", radius);
+            group.bench_with_input(id, &radius, |b, _| {
                 b.iter_with_large_drop(|| cakes.batch_rnn_search(&queries, radius, RnnAlgorithm::Clustered));
             });
         }
+
+        group.sample_size(10);
+        let id = BenchmarkId::new("Linear", radius);
+        group.bench_with_input(id, &radius, |b, _| {
+            b.iter_with_large_drop(|| cakes.batch_rnn_search(&queries, radius, RnnAlgorithm::Linear));
+        });
 
         group.finish();
     }
