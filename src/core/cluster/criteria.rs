5ac48d092e4421b9d14f8506a5a20fee8afc1ba5
Najib Ishaq <najib_ishaq@zoho.com>
2023-07-11 09:44:10 -0400

    feat: Partition Criteria can now be better composed

--- src/core/cluster/criteria.rs
+++ src/core/cluster/criteria.rs
@@ -10,7 +10,7 @@ use super::Cluster;
 ///
 /// - `T`: The type of the instances in the `Cluster`.
 /// - `U`: The type of the distance values between instances.
-pub trait PartitionCriterion<T: Send + Sync + Copy, U: Number>: std::fmt::Debug + Send + Sync {
+pub trait PartitionCriterion<T: Send + Sync + Copy, U: Number>: Send + Sync {
     /// Check whether a `Cluster` meets the criterion for partitioning.
     // TODO: Figure out how to have this not leak `Cluster` or make `Cluster` public.
     fn check(&self, c: &Cluster<T, U>) -> bool;
@@ -18,7 +18,6 @@ pub trait PartitionCriterion<T: Send + Sync + Copy, U: Number>: std::fmt::Debug 
 
 /// A collection of criteria used to decide when to partition a `Cluster`.
 #[allow(clippy::module_name_repetitions)]
-#[derive(Debug)]
 pub struct PartitionCriteria<T: Send + Sync + Copy, U: Number> {
     /// The criteria used to decide when to partition a `Cluster`.
     criteria: Vec<Box<dyn PartitionCriterion<T, U>>>,
@@ -80,9 +79,10 @@ impl<T: Send + Sync + Copy, U: Number> PartitionCriteria<T, U> {
         self.criteria.push(c);
         self
     }
+}
 
-    /// Check whether a `Cluster` meets the criteria for partitioning.
-    pub(crate) fn check(&self, cluster: &Cluster<T, U>) -> bool {
+impl<T: Send + Sync + Copy, U: Number> PartitionCriterion<T, U> for PartitionCriteria<T, U> {
+    fn check(&self, cluster: &Cluster<T, U>) -> bool {
         !cluster.is_singleton()
             && if self.check_all {
                 self.criteria.iter().all(|c| c.check(cluster))
