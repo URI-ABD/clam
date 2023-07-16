f3d483bb95600e4aca60f9937ebe82b36e9d5387
Najib Ishaq <najib_ishaq@zoho.com>
2023-07-16 17:10:39 -0600

    feat: making a workspace out of multiple projects

--- profile.sh
+++
@@ -1,7 +0,0 @@
-# Run `cargo bench --no-run` and copy the target paths we need to profile
-
-# Paste the path here
-BENCH="target/release/deps/rnn_search-59b89a1514a8c2aa"
-
-# Run this command using `bash profile.sh` to profile using samply
-samply record ./$BENCH
