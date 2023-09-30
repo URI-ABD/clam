2496aeb88bcadb311cf8230e7d2050ba7234a8d7
Najib Ishaq <najib_ishaq@zoho.com>
2023-09-30 14:39:20 -0400

    build: bumped version 0.22.0 -> 0.22.1

--- abd-clam/src/lib.rs
+++ abd-clam/src/lib.rs
@@ -31,7 +31,7 @@ pub use crate::{
 };
 
 /// The current version of the crate.
-pub const VERSION: &str = "0.22.0";
+pub const VERSION: &str = "0.22.1";
 
 /// Common distance functions and their names for slices of `f32`.
 #[allow(clippy::type_complexity)]
