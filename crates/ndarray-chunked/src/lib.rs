d620637113988e793d271110de8aeb8a943855e2
Najib Ishaq <najib_ishaq@zoho.com>
2024-02-12 15:33:47 -0500

    chore: added lints

--- crates/ndarray-chunked/src/lib.rs
+++ crates/ndarray-chunked/src/lib.rs
@@ -1,21 +1,53 @@
+#![deny(clippy::correctness)]
+#![warn(
+    missing_docs,
+    clippy::all,
+    clippy::suspicious,
+    clippy::style,
+    clippy::complexity,
+    clippy::perf,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::missing_docs_in_private_items,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::panic,
+    clippy::cast_lossless
+)]
+#![doc = include_str!("../README.md")]
+
 use pyo3::prelude::*;
 
+/// The version of the crate.
 pub const VERSION: &str = "0.1.0";
 
+/// Formats the sum of two numbers as string.
+#[must_use]
 pub fn sum_as_string(a: usize, b: usize) -> String {
     (a + b).to_string()
 }
 
 /// Formats the sum of two numbers as string.
+///
+/// # Errors
+///
+/// - If the sum of `a` and `b` is not representable as a string.
 #[pyfunction]
 #[pyo3(name = "sum_as_string")]
-fn py_sum_as_string(a: usize, b: usize) -> PyResult<String> {
+#[allow(clippy::unnecessary_wraps)]
+pub fn py_sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
 /// A Python module implemented in Rust.
+///
+/// # Errors
+///
+/// - If the module cannot be created.
+/// - If some function cannot be added to the module.
 #[pymodule]
-fn ndarray_chunked(_py: Python, m: &PyModule) -> PyResult<()> {
+#[allow(clippy::unnecessary_wraps)]
+pub fn ndarray_chunked(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_sum_as_string, m)?)?;
     Ok(())
 }
