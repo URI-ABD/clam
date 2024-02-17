7cc6718033913ae3472c63a9d47aa0b32f2de8f3
djperrone <david_perrone@uri.edu>
2024-02-16 21:59:29 -0500

    added test for saving and loading vertex tree

--- crates/abd-clam/tests/test_vertex.rs
+++ crates/abd-clam/tests/test_vertex.rs
@@ -1,8 +1,78 @@
-//! Tests for the `Vertex` struct.
-
 use abd_clam::{graph::Vertex, Cluster, PartitionCriteria, Tree};
+use abd_clam::{BaseCluster, Dataset, Instance, VecDataset};
+use distances::Number;
+use tempdir::TempDir;
 
 mod utils;
+
+type Vertexf32 = Vertex<f32>;
+type DataSetf32 = VecDataset<Vec<f32>, f32, usize>;
+type Treef32 = Tree<Vec<f32>, f32, DataSetf32, Vertexf32>;
+#[test]
+fn save_load_vertex() {
+    let data = utils::gen_dataset(1000, 10, 42, utils::euclidean);
+    let metric = data.metric();
+
+    let criteria = PartitionCriteria::default();
+    let raw_tree: Treef32 = Tree::new(data, Some(42)).partition(&criteria, Some(42));
+
+    let tree_dir = TempDir::new("tree_medium_vertex").unwrap();
+
+    // Save the tree
+    raw_tree.save(tree_dir.path()).unwrap();
+
+    // Recover the tree
+    let rec_tree: Treef32 = Tree::load(tree_dir.path(), metric, false).unwrap();
+
+    // Assert recovering was successful
+    assert_eq!(raw_tree.depth(), rec_tree.depth(), "Tree depths not equal.");
+    assert_subtree_equal(
+        raw_tree.root(),
+        raw_tree.data(),
+        rec_tree.root(),
+        rec_tree.data(),
+        metric,
+    );
+}
+
+/// Asserts that two clusters are equal.
+fn assert_subtree_equal<I: Instance, U: Number, M: Instance>(
+    raw_cluster: &Vertex<U>,
+    raw_data: &VecDataset<I, U, M>,
+    rec_cluster: &Vertex<U>,
+    rec_data: &VecDataset<I, U, M>,
+    metric: fn(&I, &I) -> U,
+) {
+    // Assert their cardinalities
+    assert_eq!(
+        raw_cluster.cardinality(),
+        rec_cluster.cardinality(),
+        "Cardinalities are not equal."
+    );
+
+    // Resolve centers
+    let (raw_center, rec_center) = (&raw_data[raw_cluster.arg_center()], &rec_data[rec_cluster.arg_center()]);
+    let (raw_radial, rec_radial) = (&raw_data[raw_cluster.arg_radial()], &rec_data[rec_cluster.arg_radial()]);
+
+    // Assert centers and radials are equal
+    assert_eq!(metric(raw_center, rec_center), U::zero(), "Centers are not equal.");
+    assert_eq!(metric(raw_radial, rec_radial), U::zero(), "Radials are not equal.");
+
+    // Get children and assert they are of equal optionality
+    let (raw_children, rec_children) = (&raw_cluster.children(), &rec_cluster.children());
+
+    match raw_children {
+        None => assert!(rec_children.is_none(), "One cluster has children, the other does not"),
+        Some([left_1, right_1]) => {
+            assert!(rec_children.is_some(), "One cluster has children, the other does not");
+
+            let [left_2, right_2] = rec_children.unwrap();
+
+            assert_subtree_equal(left_1, raw_data, left_2, rec_data, metric);
+            assert_subtree_equal(right_1, raw_data, right_2, rec_data, metric);
+        }
+    }
+}
 
 #[test]
 fn ratios() {
@@ -16,9 +86,9 @@ fn ratios() {
     let partition_criteria = PartitionCriteria::default();
     let root = Tree::<_, _, _, Vertex<_>>::new(data, Some(42)).partition(&partition_criteria, None);
 
-    //          1
-    //    10        11
-    // 100  101
+    //          1 (root)
+    //    10 (lc)       11 (rc)
+    // 100 (lclc)  101 (lcrc)
 
     let all_ratios = root
         .root()
@@ -77,9 +147,9 @@ fn ratios() {
         -1.,
     ];
     let rc_ratios = vec![
-        all_cardinalities[3] as f64 / root_ratios[0] as f64,
-        all_radius[3] as f64 / root_ratios[1] as f64,
-        all_lfd[3] / root_ratios[2],
+        all_cardinalities[4] as f64 / root_ratios[0] as f64,
+        all_radius[4] as f64 / root_ratios[1] as f64,
+        all_lfd[4] / root_ratios[2],
         -1.,
         -1.,
         -1.,
