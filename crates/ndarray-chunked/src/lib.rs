6e3eb816116fd52892c23947c44ee3ad20978941
Oliver McLaughlin <olwmcjp@gmail.com>
2024-02-26 10:56:52 -0500

    Checkpoint

--- crates/ndarray-chunked/src/lib.rs
+++ crates/ndarray-chunked/src/lib.rs
@@ -16,38 +16,41 @@
 )]
 #![doc = include_str!("../README.md")]
 
-use pyo3::prelude::*;
-
 /// The version of the crate.
 pub const VERSION: &str = "0.1.0";
 
-/// Formats the sum of two numbers as string.
-#[must_use]
-pub fn sum_as_string(a: usize, b: usize) -> String {
-    (a + b).to_string()
-}
+///
+pub mod chunked_array;
 
-/// Formats the sum of two numbers as string.
-///
-/// # Errors
-///
-/// - If the sum of `a` and `b` is not representable as a string.
-#[pyfunction]
-#[pyo3(name = "sum_as_string")]
-#[allow(clippy::unnecessary_wraps)]
-pub fn py_sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+// use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
-///
-/// # Errors
-///
-/// - If the module cannot be created.
-/// - If some function cannot be added to the module.
-#[pymodule]
-#[allow(clippy::unnecessary_wraps)]
-pub fn ndarray_chunked(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(py_sum_as_string, m)?)?;
-    Ok(())
-}
+// /// Formats the sum of two numbers as string.
+// #[must_use]
+// pub fn sum_as_string(a: usize, b: usize) -> String {
+//     (a + b).to_string()
+// }
+
+// /// Formats the sum of two numbers as string.
+// ///
+// /// # Errors
+// ///
+// /// - If the sum of `a` and `b` is not representable as a string.
+// #[pyfunction]
+// #[pyo3(name = "sum_as_string")]
+// #[allow(clippy::unnecessary_wraps)]
+// pub fn py_sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
+
+// /// A Python module implemented in Rust.
+// ///
+// /// # Errors
+// ///
+// /// - If the module cannot be created.
+// /// - If some function cannot be added to the module.
+// #[pymodule]
+// #[allow(clippy::unnecessary_wraps)]
+// pub fn ndarray_chunked(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(py_sum_as_string, m)?)?;
+//     Ok(())
+// }
