02c8c2cf7fecbf56ebe50a52acaefe7aed306d17
Najib Ishaq <najib_ishaq@zoho.com>
2024-06-20 15:22:15 -0400

    chore: added more checks for search correctness

--- crates/abd-clam/src/pancakes/mod.rs
+++ crates/abd-clam/src/pancakes/mod.rs
@@ -1,8 +1,8 @@
 //! Compression and Decompression
 
 mod codec;
-mod knn;
-mod rnn;
+pub mod knn;
+pub mod rnn;
 mod search;
 
 pub use codec::{decode_general, encode_general, CodecData, DecoderFn, EncoderFn, SquishyBall};
--- crates/pancakes-results/src/main.rs
+++ crates/pancakes-results/src/main.rs
@@ -2,11 +2,12 @@ use std::io::Write;
 use std::path::Path;
 
 use abd_clam::{
-    pancakes::{decode_general, encode_general, CodecData, SquishyBall},
-    Cluster, PartitionCriteria, VecDataset,
+    pancakes::{decode_general, encode_general, rnn, CodecData, SquishyBall},
+    Cluster, Dataset, PartitionCriteria, VecDataset,
 };
 use distances::{strings::Penalties, Number};
-use symagen::random_edits::{generate_clumped_data, generate_random_string};
+use rand::prelude::*;
+use symagen::random_edits::{are_we_there_yet, generate_clumped_data, generate_random_string};
 
 #[allow(clippy::ptr_arg)]
 fn lev_metric(x: &String, y: &String) -> u16 {
@@ -124,7 +125,11 @@ fn main() -> Result<(), String> {
         (32, 1024),
     ];
     for &(n, m) in &sizes[..] {
+        println!();
+
         let expected_name = format!("{n}-{m}.txt");
+        println!("Dataset: {expected_name}");
+
         let expected_path = dataset_dir.join(&expected_name);
         let clumped_data = if !expected_path.exists() {
             let clumped_data = generate_clumped_data(&seed_string, penalties, &alphabet, n, m, 10);
@@ -134,16 +139,82 @@ fn main() -> Result<(), String> {
             read_data(&expected_path)?
         };
 
-        let (metadata, clumped_data) = clumped_data.into_iter().unzip();
+        // Select 10 random queries from the clumped data
+        let (queries, clumped_data) = {
+            let mut clumped_data = clumped_data;
+            // Shuffle the clumped data
+            let mut rng = rand::thread_rng();
+            clumped_data.shuffle(&mut rng);
+            let queries = clumped_data
+                .iter()
+                .take(25)
+                .map(|(m, p)| {
+                    let q = are_we_there_yet(p, penalties, 5, &alphabet);
+                    (m.clone(), q)
+                })
+                .collect::<Vec<_>>();
+            (queries, clumped_data)
+        };
+
+        let (query_meta, query_data): (Vec<_>, Vec<_>) = queries.into_iter().unzip();
+        let (clumped_meta, clumped_data) = clumped_data.into_iter().unzip();
 
         let name = format!("{n}x{m}");
         let mut dataset =
-            VecDataset::new(name, clumped_data, lev_metric, true).assign_metadata(metadata)?;
+            VecDataset::new(name, clumped_data, lev_metric, true).assign_metadata(clumped_meta)?;
+
+        // Get a baseline for linear search
+        let baseline_rnn = std::time::Instant::now();
+        let hits_rnn = query_data
+            .iter()
+            .map(|q| dataset.par_linear_rnn(q, 10))
+            .collect::<Vec<_>>();
+        let baseline_rnn = baseline_rnn.elapsed().as_secs_f32();
+        let hits_rnn = hits_rnn
+            .into_iter()
+            .map(|h| {
+                h.into_iter()
+                    .map(|(i, d)| (dataset.metadata_of(i).clone(), d))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        println!(
+            "Baseline RNN: {baseline_rnn:.4}s, num_queries: {}",
+            hits_rnn.len()
+        );
+        for (qm, hits) in query_meta.iter().zip(hits_rnn.iter()) {
+            // Check that the metadata matches
+            assert!(hits.iter().any(|(m, _)| m == qm))
+        }
+
+        let baseline_knn = std::time::Instant::now();
+        let hits_knn = query_data
+            .iter()
+            .map(|q| dataset.par_linear_knn(q, 10))
+            .collect::<Vec<_>>();
+        let baseline_knn = baseline_knn.elapsed().as_secs_f32();
+        let hits_knn = hits_knn
+            .into_iter()
+            .map(|h| {
+                h.into_iter()
+                    .map(|(i, d)| (dataset.metadata_of(i).clone(), d))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        println!(
+            "Baseline KNN: {baseline_knn:.4}s, num_queries: {}",
+            hits_knn.len()
+        );
+
+        let tree_time = std::time::Instant::now();
         let criteria = PartitionCriteria::default();
         let seed = Some(42);
         let root = SquishyBall::new_root(&dataset, seed).partition(&mut dataset, &criteria, seed);
+        let tree_time = tree_time.elapsed().as_secs_f32();
         let tree_size = root.subtree().len();
+        println!("Tree built in {tree_time:.4}s");
 
+        let compression_time = std::time::Instant::now();
         let metadata = dataset.metadata().to_vec();
         let dataset = CodecData::new(
             root,
@@ -156,8 +227,11 @@ fn main() -> Result<(), String> {
         // Write the dataset to a binary file
         let bin_dir = dataset_dir.join(format!("codec-{n}-{m}"));
         dataset.save(&bin_dir)?;
+        let compression_time = compression_time.elapsed().as_secs_f32();
+        println!("Dataset compressed in {compression_time:.4}s");
 
         // Reload the dataset and tree, and check that they are the same
+        let decompression_time = std::time::Instant::now();
         let re_data = CodecData::<String, u16, String>::load(
             &bin_dir,
             lev_metric,
@@ -165,6 +239,9 @@ fn main() -> Result<(), String> {
             encode_general::<u16>,
             decode_general,
         )?;
+        let decompression_time = decompression_time.elapsed().as_secs_f32();
+        println!("Dataset decompressed in {decompression_time:.4}s");
+
         assert_eq!(dataset.root().subtree(), re_data.root().subtree());
         assert_eq!(dataset.centers(), re_data.centers());
         assert_eq!(dataset.metadata(), re_data.metadata());
@@ -184,6 +261,54 @@ fn main() -> Result<(), String> {
             assert_eq!(c.unitary_cost(), rc.unitary_cost());
         }
 
+        // Run the queries on the reloaded dataset
+        let codec_rnn = std::time::Instant::now();
+        let codec_hits_rnn = query_data
+            .iter()
+            .map(|q| re_data.rnn_search(q, 10, &rnn::Algorithm::Clustered))
+            .collect::<Vec<_>>();
+        let codec_rnn = codec_rnn.elapsed().as_secs_f32();
+        let codec_hits_rnn = codec_hits_rnn
+            .into_iter()
+            .map(|h| {
+                h.into_iter()
+                    .map(|(i, d)| (re_data.metadata()[i].clone(), d))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        // Check that the hits are the same
+        for ((qm, codec_hits), hits) in query_meta
+            .iter()
+            .zip(codec_hits_rnn.iter())
+            .zip(hits_rnn.iter())
+        {
+            // Check that the metadata matches
+            assert!(codec_hits.iter().any(|(m, _)| m == qm));
+
+            // Check that the number of hits is the same
+            assert_eq!(codec_hits.len(), hits.len());
+
+            // Sort the hits by metadata
+            let mut codec_hits = codec_hits.clone();
+            codec_hits.sort_by(|(m1, _), (m2, _)| m1.cmp(m2));
+            let mut hits = hits.clone();
+            hits.sort_by(|(m1, _), (m2, _)| m1.cmp(m2));
+
+            // Check that the hits are the same
+            for ((m1, d1), (m2, d2)) in codec_hits.iter().zip(hits.iter()) {
+                assert_eq!(m1, m2);
+                assert_eq!(d1, d2);
+            }
+        }
+        println!(
+            "Codec RNN: {codec_rnn:.4}s, num_queries: {}",
+            codec_hits_rnn.len()
+        );
+        println!(
+            "RNN ratio: baseline / codec = {:.4}",
+            baseline_rnn / codec_rnn
+        );
+
         // Get the size of the binary files in the codec directory
         let bin_size = bin_dir
             .read_dir()
@@ -197,7 +322,7 @@ fn main() -> Result<(), String> {
         let trimmed_size = dataset.root().compressible_subtree().len();
 
         println!(
-            "{expected_name}, Root: {}, Clusters: {tree_size}, Trimmed: {trimmed_size}",
+            "Root: {}, Clusters: {tree_size}, Trimmed: {trimmed_size}",
             dataset.root().name()
         );
 
@@ -212,8 +337,6 @@ fn main() -> Result<(), String> {
             "File sizes: Text: {txt_size}, Binary: {bin_size}, Actual Factor: {:.2e}",
             bin_size.as_f64() / txt_size.as_f64()
         );
-
-        println!();
     }
 
     Ok(())
