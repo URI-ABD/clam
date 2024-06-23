7cee11095e7630586c39e29f0b52df5c63ffa439
brianmaterne <brian_materne@LAPTOP-KT6J22CL>
2024-06-22 20:19:10 -0400

    Everything from old distances added, dice functionality added

--- crates/distances/tests/test_sets.rs
+++ crates/distances/tests/test_sets.rs
@@ -1,4 +1,6 @@
 use distances::sets::dice;
+use distances::sets::jaccard;
+use distances::sets::kulsinski;
 
 #[test]
 #[allow(clippy::float_equality_without_abs)]
@@ -26,4 +28,93 @@ fn test_dice() {
 
     let distance: f32 = dice(&x, &y);
     assert!((distance - 0.8) < f32::EPSILON);
+}
+
+/// Generates a length 1000 set with each bit flipped randomly on or off
+fn gen_set() -> Vec<u16> {
+    let mut vec = Vec::new();
+    for i in 0..1000 {
+        if rand::random() {
+            vec.push(i);
+        }
+    }
+    vec
+}
+
+/// Random exhaustive testing of set distances, manually creating union and intersection values
+#[test]
+fn sets_test() {
+    for _ in 0..10000 {
+        let x: Vec<u16> = gen_set();
+        let y: Vec<u16> = gen_set();
+        let mut union: f32 = 0.0;
+        let mut intersection: f32 = 0.0;
+        let mut size: f32 = 0.0;
+        for i in 0_u16..1000 {
+            if x.contains(&i) || y.contains(&i) {
+                union += 1.0;
+                size += 1.0;
+            }
+            if x.contains(&i) && y.contains(&i) {
+                intersection += 1.0;
+                size += 1.0;
+            }
+        }
+        let mut distance: f32;
+        let mut real_distance: f32;
+
+        distance = jaccard(&x, &y);
+        if union == 0.0 {
+            real_distance = 0.0;
+        } else {
+            real_distance = 1_f32 - intersection / union;
+        }
+        assert!((distance - real_distance).abs() < f32::EPSILON);
+
+        distance = dice(&x, &y);
+        if union == 0.0 {
+            real_distance = 0.0;
+        } else {
+            real_distance = 1_f32 - (2_f32 * (intersection / size));
+        }
+        assert!((distance - real_distance).abs() < f32::EPSILON);
+
+        distance = kulsinski(&x, &y);
+        if union == 0.0 {
+            real_distance = 0.0;
+        } else {
+            real_distance = 1_f32 - intersection / (union + union - intersection);
+        }
+        assert!((distance - real_distance).abs() < f32::EPSILON);
+    }
+}
+
+/// Boundary testing for set distances, equal sets or one zero set
+#[test]
+fn bounds_test() {
+    let x: Vec<u16> = gen_set();
+    let y: Vec<u16> = Vec::new();
+
+    let mut distance: f32;
+
+    distance = jaccard(&x, &x);
+    assert!(distance < f32::EPSILON);
+    distance = jaccard(&x, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
+    distance = jaccard(&y, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
+
+    distance = dice(&x, &x);
+    assert!(distance < f32::EPSILON);
+    distance = dice(&x, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
+    distance = dice(&y, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
+
+    distance = kulsinski(&x, &x);
+    assert!(distance < f32::EPSILON);
+    distance = kulsinski(&x, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
+    distance = kulsinski(&y, &y);
+    assert!(distance - 1.0 < f32::EPSILON);
 }
