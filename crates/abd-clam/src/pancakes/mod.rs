b3b42734a17aa9e721465d88c37c73cb9a560e71
Najib Ishaq <najib_ishaq@zoho.com>
2024-08-10 12:47:26 -0400

    chore: deleted old pancakes code

--- crates/abd-clam/src/pancakes/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Compression and Decompression
-
-mod codec;
-pub mod knn;
-pub mod rnn;
-mod search;
-
-pub use codec::{decode_general, encode_general, CodecData, DecoderFn, EncoderFn, SquishyBall};
